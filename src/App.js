00a7a0714d90715955770ee059b5ff4ad440cd81
Rocket <jyp@Rocket.local>
2023-02-13 22:37:33 +0900

    get bestseller api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import { Outlet } from 'react-router';
 import './App.css';
+import Navbar from './components/ui/Navbar';
 
 function App() {
   return (
-    <div>
-      <h1>Hello world:)</h1>
-      <span>release 0.2</span>
-      <input type="text" placeholder="Login" />
-    </div>
+    <>
+      <Navbar />
+      <Outlet />
+    </>
   );
 }
 
