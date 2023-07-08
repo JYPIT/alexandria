7d10156facdf0b2934a73ec6caf1086fbcac261a
JYP <rocket.jyp@gmail.com>
2023-07-08 23:36:11 +0900

    deploy CLOUDTYPE_v1

--- backend/db/database.js
+++ backend/db/database.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 // docker test
 dotenv.config();
 
+// LOCAL DEV
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.REACT_APP_MYSQL_HOST,
@@ -11,5 +12,15 @@ const pool = mysql.createPool({
   password: process.env.REACT_APP_MYSQL_PASSWORD,
   database: process.env.REACT_APP_MYSQL_DATABASE,
 });
+
+// AWS
+// const pool = mysql.createPool({
+//   connectionLimit: 10,
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_ROOT_PASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+//   port: process.env.MYSQL_PORT,
+// });
 
 export const db = pool.promise();
