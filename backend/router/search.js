776119f726f054fc8a37ae49f2478b61fab18a45
JYP <rocket.jyp@gmail.com>
2023-07-09 13:33:37 +0900

    [CLOUDTYPE] serach test

--- backend/router/search.js
+++ backend/router/search.js
@@ -9,11 +9,12 @@ const API_KEY = process.env.REACT_APP_ALADIN_API_KEY;
 
 router.get('/', async (req, res, next) => {
   const query = req.query.search_query;
-
+  console.log(query);
   const searched = await fetch(
     `${BASE_URL}/ItemSearch.aspx?ttbkey=${API_KEY}&Query=${query}&QueryType=Keyword&Cover=Big&MaxResults=6&start=1&SearchTarget=Book&output=js&Version=20131101`
   ).then((res) => res.json());
   if (searched) {
+    console.log(searched.item);
     res.status(200).json(searched.item);
   } else {
     res.status(404).json({ message: '검색어가 존재하지 않습니다.' });
